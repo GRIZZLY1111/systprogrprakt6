57e690470176d91c7a1a42256071993993da02f7
GRIZZLY1111 <msolihov28@gmail.com>
2025-10-25 12:39:15 +0500

    Add files via upload

--- Prakt5Dop/Prakt5Dop/Prakt5Dop.cpp
+++ Prakt5Dop/Prakt5Dop/Prakt5Dop.cpp
@@ -3,35 +3,39 @@
 
 #include <iostream>
 struct EventSystem {
-    void (*handlers[100])();
+    void (*handlers[100])(const std::string& );
     int count = 0;
 };
-void onUserLogin() {
-    std::cout << "Пользователь вошёл в систему" << std::endl;
+void onUserLogin(const std::string& message) {
+    std::cout << "Пользователь вошёл в систему" << message << std::endl;
 };
-void onUserLogout() {
-    std::cout << "Пользователь вышел из системы" << std::endl;
+void onUserLogout(const std::string& message) {
+    std::cout << "Пользователь вышел из системы" << message << std::endl;
 };
-void onError() {
-    std::cout << "Произошла ошибка" << std::endl;
+void onError(const std::string& message) {
+    std::cout << "Произошла ошибка" << message <<std::endl;
 };
-void registerHandler(EventSystem& system, void(*handler)()) {
+void registerHandler(EventSystem& system, void(*handler)(const std::string& message)) {
+
     system.handlers[system.count] = handler;
     system.count += 1;
 }
-void triggerEvent(const EventSystem& system){
+void handler(const std::string& message) {
+
+}
+void triggerEvent(const EventSystem& system, const std::string& message){
     for (int i = 0; i < 3; i++) {
-        system.handlers[i]();
+        system.handlers[i](message);
     }
 }
 int main()
 {
     setlocale(0, "rus");
     EventSystem system;
-    registerHandler(system, &onUserLogin);
-    registerHandler(system, &onUserLogout);
-    registerHandler(system, &onError);
-    triggerEvent(system);
+    registerHandler(system, onUserLogin);
+    registerHandler(system, onUserLogout);
+    registerHandler(system, onError);
+    triggerEvent(system," //Обработчик с аргументом");
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
